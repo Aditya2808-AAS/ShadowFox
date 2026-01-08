c15e991f0d02c390feed1cea8268adfa5fcbd5f4
Aditya2808-AAS <adityasapte10@gmail.com>
2026-01-08 14:46:10 +0530

    Added Task 2 - Conditions

---
+++ Task1_Variables/Task2_Conditions/conditions.py
@@ -0,0 +1,27 @@
+# Task 2: Conditions
+
+# Example 1: Check if a number is positive, negative or zero
+number = 7
+
+if number > 0:
+    print("Number is positive")
+elif number < 0:
+    print("Number is negative")
+else:
+    print("Number is zero")
+
+# Example 2: Voting eligibility
+age = 18
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+# Example 3: Check if number is even or odd
+num = 9
+
+if num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")

aab8b6295308dedfe913ba0858bfdc2b5d03e5e7
Aditya2808-AAS <adityasapte10@gmail.com>
2026-01-08 14:09:59 +0530

    Added Task 1 - Variables

---
+++ Task1_Variables/variables.py
@@ -0,0 +1,17 @@
+# Task 1: Variables
+
+# 1. Variable pi
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+# 2. Reserved keyword example
+# for = 4  # This will give an error because 'for' is a Python keyword
+
+# 3. Simple Interest Calculation
+principal = 1000
+rate = 5
+time = 3
+
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest:", simple_interest)
